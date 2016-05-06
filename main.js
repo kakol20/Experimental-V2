2f3fc85755e8724188ce349d203dbd51495c03b0
kakol20 <adrianaquino.aquino33@gmail.com>
2016-05-06 19:39:14 +0100

    Trying to get randomisation as close as possible to a year

--- main.js
+++ main.js
@@ -86,11 +86,11 @@ var getPrimes = function() {
 
 var convertHour = function() {
 	var hourToConvert = prompt("Enter a positive number or leave blank for a random number");
-	hourToConvert = hourToConvert || random(12 * 4 * 7 * 24);
+	hourToConvert = hourToConvert || random(365 * 24);
 
 	while (isString(hourToConvert)) {
 		hourToConvert = prompt("Enter a positive number or leave blank for a random number");
-		hourToConvert = hourToConvert || random(12 * 4 * 7 * 24);
+		hourToConvert = hourToConvert || random(365 * 24);
 	}
 	
 	var weekToConvert = hourToConvert / (24 * 7);
@@ -168,7 +168,7 @@ var timeTill = function() {
 		var selectDate = date || "random";
 
 		if (selectDate === "random") {
-			date = currentDate.getTime() + (12 * 4 * 7 * 24 * 60 * 60 * 1000);
+			date = currentDate.getTime() + (365 * 24 * 60 * 60 * 1000);
 
 			var randomisedDate = random(date, currentDate.getTime());
 
