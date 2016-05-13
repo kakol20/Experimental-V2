ec032d5f7eb1424093a513b009de632a88ac5725
kakol20 <adrianaquino.aquino33@gmail.com>
2016-05-13 21:38:46 +0100

    Typo fix 2

--- main.js
+++ main.js
@@ -623,7 +623,7 @@ var normalDistribution = function() {
 		var t = 1 / (1 + 0.2316419 * Math.abs(x));
         var d = 0.3989423 * Math.exp(-x * x / 2);
         var prob = d * t * (0.3193815 + t * (-0.3565638 + t * (1.781478 + t * (-1.821256 + t * 1.330274))));
-        if (X > 0) {
+        if (x > 0) {
             prob = 1 - prob;
         }
         return prob;
