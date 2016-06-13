7764ef58bf893d5c5ffd01b69a8aedb832c0aac4
kakol20 <adrianaquino.aquino33@gmail.com>
2016-06-13 22:49:10 +0100

    Change output

--- main.js
+++ main.js
@@ -567,9 +567,9 @@ var normalDistribution = function() {
 
 	val = keystone.round(val, "nearest", 2);
 	mean = keystone.round(mean, "up", 2);
-	var sdSquared = keystone.round(sd * sd, "nearest", 2);
+	//var sdSquared = keystone.round(sd * sd, "nearest", 2);
 
-	document.getElementById('normalDF').innerHTML = "X ~ N(" + mean + ", " + sdSquared + ") --> P(X < " + val + ") = " + result;
+	document.getElementById('normalDF').innerHTML = "X ~ N(" + mean + ", " + keystone.round(sd * sd, "nearest", 2) + "²) --> P(X < " + val + ") = " + result;
 	console.log("Standard Deviation: " + keystone.round(sd, "nearest", 2));
 	console.log("P(Z < " + keystone.round((val - mean) / sd, "nearest", 2) + ")");
 };
